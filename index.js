8bd5579c3c0f50a0d219d09a43fb3228d6823634
Suryaprakash-G26 <gsurya124@gmail.com>
2023-12-02 20:43:35 +0530

    minor chane

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { RoomsRouter } from "./routes/rooms.js";
 import { bookingsRouter } from "./routes/booking.js";
+import { Display } from "./routes/normal.js";
 
 const app = express();
 
 app.use(express.json());
 
 const Port = process.env.Port || 8500;
-
+app.use('/',Display)
 app.use('/room', RoomsRouter);
 app.use('/booking', bookingsRouter);  
 
